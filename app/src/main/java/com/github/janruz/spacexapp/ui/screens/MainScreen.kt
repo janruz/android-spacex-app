d867ff3dad0516fd4ea39c5f7ded316175b74b3a
Jan Ruzicka <ho.ruz@seznam.cz>
2022-08-18 21:19:42 +0200

    Show SpaceX logo in TopBar

--- app/src/main/java/com/github/janruz/spacexapp/ui/screens/MainScreen.kt
+++ app/src/main/java/com/github/janruz/spacexapp/ui/screens/MainScreen.kt
@@ -1,17 +1,22 @@
 package com.github.janruz.spacexapp.ui.screens
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.janruz.spacexapp.R
@@ -36,8 +41,14 @@ fun MainScreen(
 
     Scaffold(
         topBar = {
-            TopAppBar {
-                Text(stringResource(id = R.string.rockets_title))
+            TopAppBar(
+                backgroundColor = if(isSystemInDarkTheme()) Color.Black else Color.White
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.spacex_logo),
+                    contentDescription = "",
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
     ) { paddingValues ->
