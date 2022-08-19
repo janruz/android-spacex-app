a529f4a10ef2b2ad3bc1e0013a69fb0e52a1c1e3
Jan Ruzicka <ho.ruz@seznam.cz>
2022-08-19 17:45:57 +0200

    Add Company model

---
+++ app/src/main/java/com/github/janruz/spacexapp/data/models/Company.kt
@@ -0,0 +1,65 @@
+package com.github.janruz.spacexapp.data.models
+
+import com.squareup.moshi.Json
+
+data class Company(
+    val id: String,
+    val founder: String,
+    val founded: Int,
+    val employees: Int,
+    val launchSites: Int,
+    val ceo: String,
+    val cto: String,
+    val coo: String,
+    val valuation: Long,
+    val summary: String
+)
+
+data class CompanyFromApi(
+
+    @field:Json(name = "id")
+    val id: String,
+
+    @field:Json(name = "founder")
+    val founder: String,
+
+    @field:Json(name = "founded")
+    val founded: Int,
+
+    @field:Json(name = "employees")
+    val employees: Int,
+
+    @field:Json(name = "launch_sites")
+    val launchSites: Int,
+
+    @field:Json(name = "ceo")
+    val ceo: String,
+
+    @field:Json(name = "cto")
+    val cto: String,
+
+    @field:Json(name = "coo")
+    val coo: String,
+
+    @field:Json(name = "valuation")
+    val valuation: Long,
+
+    @field:Json(name = "summary")
+    val summary: String
+)
+
+val CompanyFromApi.asCompany: Company get() {
+    return Company(
+        id = this.id,
+        founder = this.founder,
+        founded = this.founded,
+        employees = this.employees,
+        launchSites = this.launchSites,
+        ceo = this.ceo,
+        cto = this.cto,
+        coo = this.coo,
+        valuation = this.valuation,
+        summary = this.summary
+
+    )
+}
