454c83cb25e615af50cfd5cfd2aa67bbef0c78e4
Jan Ruzicka <ho.ruz@seznam.cz>
2022-08-25 22:55:00 +0200

    Move MainActivity to UI package

--- app/src/main/java/com/github/janruz/spacexapp/ui/MainActivity.kt
+++ app/src/main/java/com/github/janruz/spacexapp/ui/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.github.janruz.spacexapp
+package com.github.janruz.spacexapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
 import com.github.janruz.spacexapp.ui.screens.MainScreen
 import com.github.janruz.spacexapp.ui.theme.SpaceXAppTheme
-import com.github.janruz.spacexapp.viewmodels.RocketsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
