7db2440c8b711eae27c50c01e0c2c8463dc72054
Jan Ruzicka <ho.ruz@seznam.cz>
2022-08-26 16:32:25 +0200

    Trigger select even on radio button text click

--- app/src/main/java/com/github/janruz/spacexapp/ui/components/RadioTextButton.kt
+++ app/src/main/java/com/github/janruz/spacexapp/ui/components/RadioTextButton.kt
@@ -1,10 +1,13 @@
 package com.github.janruz.spacexapp.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -28,7 +31,14 @@ fun RadioTextButton(
         )
         Text(
             text,
-            color = MaterialTheme.colors.onBackground
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) {
+                    onSelect()
+                }
         )
     }
 }
